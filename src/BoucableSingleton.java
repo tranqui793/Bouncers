0b2a3ebe7ee15ff6204bcfad54d1e227273357f5
olliens <oussama.lagha@heig-vd.ch>
2019-03-12 13:15:18 +0100

    modif de igitignore

---
+++ src/BoucableSingleton.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.image.BufferedImage;
+
+public class BoucableSingleton implements Displayer {
+    private static final BoucableSingleton singletonInstance= new BoucableSingleton();
+
+    private final JFrame bouncableFrame;
+    private final JPanel bouncableJPanel;
+    private BufferedImage bufImg;
+    private final int PREFERED_SIZE_FRAME =700;
+
+    private BoucableSingleton(){
+        bouncableFrame =new JFrame("Bouncers");
+        bouncableJPanel =new JPanel();
+
+        bouncableFrame.getContentPane().add(bouncableJPanel);
+        bouncableFrame.setResizable(true);
+        bouncableFrame.setMinimumSize(new Dimension(100, 100));
+
+        bouncableFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bouncableFrame.pack();
+        bouncableFrame.setVisible(true);
+        bufImg= (BufferedImage) bouncableJPanel.createImage(PREFERED_SIZE_FRAME,PREFERED_SIZE_FRAME);
+        bufImg.createGraphics().drawImage(bufImg,0,0,null);
+    }
+    public static BoucableSingleton getInstance(){
+        return singletonInstance;
+    }
+
+    @Override
+    public int getWidth() {
+        return bouncableJPanel.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return bouncableJPanel.getHeight();
+    }
+
+    @Override
+    public Graphics2D getGraphics() {
+        return (Graphics2D) bufImg.getGraphics();
+    }
+
+    @Override
+    public void repaint() {
+
+        bouncableJPanel.getGraphics().drawImage(bufImg, 0, 0, null);
+        bufImg = (BufferedImage) bouncableJPanel.createImage(this.getWidth(), this.getHeight());
+    }
+
+    @Override
+    public void setTitle(String s) {
+        bouncableFrame.setTitle(s);
+    }
+
+    @Override
+    public void addKeyListener(KeyAdapter ka) {
+        bouncableFrame.addKeyListener(ka);
+    }
+}
--- src/BounceApp.java
+++ src/BounceApp.java
@@ -3,12 +3,21 @@ import java.util.LinkedList;
 public class BounceApp
 {
     private LinkedList<Bouncable> bouncers;
+    private static final BoucableSingleton bou=BoucableSingleton.getInstance();
     // Autres attributs
     public BounceApp() {
         /* ... */
     }
     public void loop() {
-        /* ... */
+        while(true)
+        {
+            for(int i=0;i<bouncers.size();i++)
+            {
+                bouncers.get(i).move();
+                bouncers.get(i).draw();
+            }
+            bou.repaint();
+        }
     }
     public static void main(String ... args) {
         new BounceApp().loop();
